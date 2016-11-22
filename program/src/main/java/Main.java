7f36359c80c73c56268cb54f819d7ef8b3ba28b3
Igor Brodt <programist.igor@rambler.ru>
2016-11-23 02:31:32 +0300

    Prepare commit

---
+++ program/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.lang.String;
+import java.lang.System;
+
+class Main {
+    public static void main(String[] args) throws IOException
+    {
+        System.out.print("Hi, Igor!");
+    }
+}
