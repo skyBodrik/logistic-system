aa1e4c50ef7cd30338e65e5cc4a69c32a139a0cc
Igor Brodt <programist.igor@rambler.ru>
2016-12-23 04:17:10 +0300

    Добавлены диаграммы. Включен API

--- program/src/main/java/Main.java
+++ program/src/main/java/Main.java
@@ -4,8 +4,6 @@ import java.lang.String;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import logistic.gui.LoginForm;
-import logistic.models.Order;
-import logistic.repositories.OrdersRepository;
 import logistic.services.API;
 
 public class Main extends Application {
@@ -21,9 +19,7 @@ public class Main extends Application {
 
     public static void main(String[] args) throws IOException
     {
-        Order order = OrdersRepository.getInstance().getAll().stream().filter(p -> p.getId() == 1).findFirst().get();
-        System.out.print(order.getDetails());
-        //API.start();
+        API.start();
         launch(args);
     }
 }
